4797a90db0a88895c516d4e3726e153c874eaa1e
hareeshcs33 <hareeshcs33@gmail.com>
2020-08-08 21:08:26 +0530

    get and set methods added.

--- src/components/home.vue
+++ src/components/home.vue
@@ -7,15 +7,33 @@
         <hr/>
         <counter></counter>
         <AnotherCounter></AnotherCounter>
+        <hr/>
+        <input type="text" v-model="value" >
+        {{value}}
     </div>
 </template>
 <script>
 import Counter from './counter.vue';
 import Result from './result.vue';
 import AnotherResult from './AnotherResult.vue';
-import AnotherCounter from './AnotherCounter.vue';
+import AnotherCounter from './AnotherCounter';
 
 export default {
+    computed: {
+        value: {
+            get(){
+                return this.$store.getters.value;
+            },
+            set(value){
+                this.$store.dispatch('updateValue', value);
+            }
+        }
+    },
+    methods: {
+        updateValue(event) {
+            this.$store.dispatch('updateValue', event.torget.value);
+        }
+    },
     components: {
         Counter,
         Result,
