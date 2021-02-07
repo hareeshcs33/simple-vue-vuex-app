4658f74b0a3282859ade97ebbace154de8306cc6
hareeshcs33 <hareeshcs33@gmail.com>
2021-02-07 16:49:29 +0530

    UI update

--- src/user/user.vue
+++ src/user/user.vue
@@ -2,27 +2,45 @@
   <div class="container">
     <div class="row">
       <user-item
-        v-for="(data, index) in userData"
+        v-for="(data, index) in filteredUserData"
         :data="data"
         :key="data.id"
         :index="index"
       ></user-item>
+    </div>
+    <div class="d-flex align-items-center justify-content-end">
+      <button class="btn btn-outline-primary px-5 mr-3" @click="prevList">
+        Prev
+      </button>
+      <button class="btn btn-outline-primary px-5" @click="nextList">
+        Next
+      </button>
     </div>
   </div>
 </template>
 <script>
 import UserItem from "./user-item.vue";
 export default {
-  components: {
-    UserItem
-  },
   data() {
     return {
-      userData: null
+      userData: null,
+      userDataItems: 0
     };
   },
-  methods: {},
-  computed: {},
+  methods: {
+    prevList() {
+      console.log("prev list");
+    },
+    nextList() {
+      console.log("next list");
+      console.log(this.userData);
+    }
+  },
+  computed: {
+    filteredUserData() {
+      return this.userData;
+    }
+  },
   mounted() {
     axios
       .get("https://jsonplaceholder.typicode.com/posts", {
@@ -32,6 +50,9 @@ export default {
       })
       .then(response => (this.userData = response.data))
       .catch(err => console.log(err));
+  },
+  components: {
+    UserItem
   }
 };
 </script>
